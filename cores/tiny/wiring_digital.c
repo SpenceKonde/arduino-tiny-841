474f278f3325445adeffb05cd5b569383d3dbf52
Spence Konde <spencekonde@gmail.com>
2015-02-28 05:36:40 -0500

    Fix #2
    If we have something that uses the pullup enable register, use new version of pinMode() and digitalWrite()

--- cores/tiny/wiring_digital.c
+++ cores/tiny/wiring_digital.c
@@ -23,6 +23,7 @@
 
   Modified 28-08-2009 for attiny84 R.Wiersma
   Modified 14-10-2009 for attiny45 Saposoft
+  Modified 26-02-2015 for attiny841 and other parts that use the PUEx registers. 
 */
 
 #include "wiring_private.h"
@@ -31,37 +32,77 @@
 #include "core_timers.h"
 #include "PwmTimer.h"
 
+#if defined( USE_PUE_REGISTER )
 void pinMode(uint8_t pin, uint8_t mode)
 {
-	uint8_t bit = digitalPinToBitMask(pin);
-	uint8_t port = digitalPinToPort(pin);
-	volatile uint8_t *reg, *out;
+  uint8_t bit = digitalPinToBitMask(pin);
+  uint8_t port = digitalPinToPort(pin);
+  volatile uint8_t *reg, *out, *pue;
 
-	if (port == NOT_A_PIN) return;
+  if (port == NOT_A_PIN) return;
 
-	// JWS: can I let the optimizer do this?
-	reg = portModeRegister(port);
+  // JWS: can I let the optimizer do this?
+  reg = portModeRegister(port);
+  out = portOutputRegister(port);
+  pue = portPullupRegister(port);
+
+  if (mode == INPUT) { 
+    uint8_t oldSREG = SREG;
+                cli();
+    *reg &= ~bit;
+    *out &= ~bit;
+    *pue &= ~bit;
+    SREG = oldSREG;
+  } else if (mode == INPUT_PULLUP) {
+    uint8_t oldSREG = SREG;
+                cli();
+    *reg &= ~bit;
+    *out |= bit;
+    *pue |= bit;
+    SREG = oldSREG;
+  } else {
+    uint8_t oldSREG = SREG;
+                cli();
+    *pue &= ~bit;
+    *reg |= bit;
+    SREG = oldSREG;
+  }
+}
+#else 
+
+void pinMode(uint8_t pin, uint8_t mode)
+{
+  uint8_t bit = digitalPinToBitMask(pin);
+  uint8_t port = digitalPinToPort(pin);
+  volatile uint8_t *reg, *out;
+
+  if (port == NOT_A_PIN) return;
+
+  // JWS: can I let the optimizer do this?
+  reg = portModeRegister(port);
   out = portOutputRegister(port);
 
-	if (mode == INPUT) {
-		uint8_t oldSREG = SREG;
-    cli();
-		*reg &= ~bit;
-    *out &= ~bit;
-		SREG = oldSREG;
-	} else if (mode == INPUT_PULLUP) {
+  if (mode == INPUT) { 
     uint8_t oldSREG = SREG;
-    cli();
+                cli();
+    *reg &= ~bit;
+    *out &= ~bit;
+    SREG = oldSREG;
+  } else if (mode == INPUT_PULLUP) {
+    uint8_t oldSREG = SREG;
+                cli();
     *reg &= ~bit;
     *out |= bit;
     SREG = oldSREG;
-	} else {
-		uint8_t oldSREG = SREG;
-    cli();
-		*reg |= bit;
-		SREG = oldSREG;
-	}
+  } else {
+    uint8_t oldSREG = SREG;
+                cli();
+    *reg |= bit;
+    SREG = oldSREG;
+  }
 }
+
+#endif
 
 // Forcing this inline keeps the callers from having to push their own stuff
 // on the stack. It is a good performance win and only takes 1 more byte per
@@ -118,44 +159,76 @@ __attribute__((always_inline)) static inline void turnOffPWM( uint8_t pin )
     {
       Pwm5_SetCompareOutputMode( Pwm5_Disconnected );
     }
-    else
+    //else
   #endif
 
   #if CORE_PWM_COUNT >= 7
   #error Only 6 PWM pins are supported.  Add more conditions.
   #endif
-
-    {
+{
     }
-
 }
+
+#if defined( USE_PUE_REGISTER )
 
 void digitalWrite(uint8_t pin, uint8_t val)
 {
-	uint8_t bit = digitalPinToBitMask(pin);
-	uint8_t port = digitalPinToPort(pin);
-	volatile uint8_t *out;
+  uint8_t bit = digitalPinToBitMask(pin);
+  uint8_t port = digitalPinToPort(pin);
+  volatile uint8_t *out, *pue;
 
-	if (port == NOT_A_PIN) return;
+  if (port == NOT_A_PIN) return;
 
-	// If the pin that support PWM output, we need to turn it off
-	// before doing a digital write.
+  // If the pin that support PWM output, we need to turn it off
+  // before doing a digital write.
   turnOffPWM( pin );
 
-	out = portOutputRegister(port);
+  out = portOutputRegister(port);
+  pue = portPullupRegister(port);
 
-	if (val == LOW) {
-		uint8_t oldSREG = SREG;
+  if (val == LOW) {
+    uint8_t oldSREG = SREG;
     cli();
-		*out &= ~bit;
-		SREG = oldSREG;
-	} else {
-		uint8_t oldSREG = SREG;
+    *out &= ~bit;
+    *pue &= ~bit; //Turn off the pullups
+    SREG = oldSREG;
+  } else {
+    uint8_t oldSREG = SREG;
     cli();
-		*out |= bit;
-		SREG = oldSREG;
-	}
+    *out |= bit;
+    *pue &= ~bit;
+    SREG = oldSREG;
+  }
 }
+#else 
+
+void digitalWrite(uint8_t pin, uint8_t val)
+{
+  uint8_t bit = digitalPinToBitMask(pin);
+  uint8_t port = digitalPinToPort(pin);
+  volatile uint8_t *out;
+
+  if (port == NOT_A_PIN) return;
+
+  // If the pin that support PWM output, we need to turn it off
+  // before doing a digital write.
+  turnOffPWM( pin );
+
+  out = portOutputRegister(port);
+
+  if (val == LOW) {
+    uint8_t oldSREG = SREG;
+    cli();
+    *out &= ~bit;
+    SREG = oldSREG;
+  } else {
+    uint8_t oldSREG = SREG;
+    cli();
+    *out |= bit;
+    SREG = oldSREG;
+  }
+}
+#endif
 
 int digitalRead(uint8_t pin)
 {
