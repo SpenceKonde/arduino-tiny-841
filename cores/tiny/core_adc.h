c27b1b2320d006d6b2ce9291d80681af48c40b95
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2015-03-25 14:54:10 -0400

    Add some fixes from 1634 core
    Including some code that might eventually be used to support 1634 on the same core.

--- cores/tiny/core_adc.h
+++ cores/tiny/core_adc.h
@@ -35,7 +35,7 @@
   Some common things
 =============================================================================*/
 
-#if defined( __AVR_ATtinyX4__ ) || defined( __AVR_ATtinyX5__ ) || defined( __AVR_ATtinyX41__ )
+#if defined( __AVR_ATtinyX4__ ) || defined( __AVR_ATtinyX5__ ) ||defined(__AVR_ATtiny1634) || defined( __AVR_ATtinyX41__ )
 
 /*
   From the '841, '84, and '85 datasheets... By default, the successive approximation
@@ -44,6 +44,9 @@
 */
 #if F_CPU == 16000000
   // 16 MHz / 128 = 125 KHz
+  #define ADC_ARDUINO_PRESCALER   ADC_Prescaler_Value_128
+#elif F_CPU == 12000000
+  // 12 MHz / 128 = 93.75 KHz
   #define ADC_ARDUINO_PRESCALER   ADC_Prescaler_Value_128
 #elif F_CPU == 8000000
   // 8 MHz / 64 = 125 KHz
@@ -317,6 +320,69 @@ typedef enum
   ADC_Input_ADC4            = B1111   // For temperature sensor.
 }
 adc_ic_t;
+
+__attribute__((always_inline)) static inline void ADC_SetInputChannel( adc_ic_t ic )
+{
+  ADMUX = (ADMUX & ~MASK4(MUX3,MUX2,MUX1,MUX0)) | (ic << MUX0);
+}
+
+__attribute__((always_inline)) static inline void ADC_StartConversion( void )
+{
+  ADCSRA |= MASK1( ADSC );
+}
+
+__attribute__((always_inline)) static inline uint8_t ADC_ConversionInProgress( void )
+{
+  return( (ADCSRA & (1<<ADSC)) != 0 );
+}
+
+__attribute__((always_inline)) static inline uint16_t ADC_GetDataRegister( void )
+{
+  return( ADC );
+}
+
+#endif
+
+
+/*=============================================================================
+  Veneer for the ATtiny1634 ADC
+=============================================================================*/
+
+typedef enum
+{
+  ADC_Reference_VCC                             = B00,
+  ADC_Reference_External                        = B01,
+  ADC_Reference_Internal_1p1                    = B10,
+  ADC_Reference_Reserved_1                      = B11
+}
+adc_vr_t;
+
+typedef enum
+{
+  ADC_Input_ADC0            = B0000,
+  ADC_Input_ADC1            = B0001,
+  ADC_Input_ADC2            = B0010,
+  ADC_Input_ADC3            = B0011,
+  ADC_Input_ADC4            = B0100,
+  ADC_Input_ADC5            = B0101,
+  ADC_Input_ADC6            = B0110,
+  ADC_Input_ADC7            = B0111,
+  ADC_Input_ADC8            = B0111,
+  ADC_Input_ADC9            = B0111,
+  ADC_Input_ADC10           = B0111,
+  ADC_Input_ADC11           = B0111,
+
+  ADC_Input_GND             = B1100,  // 0V (AGND)
+  ADC_Input_1p1             = B1101,  // 1.1V (I Ref)
+  ADC_Input_ADC12           = B1110,  // For temperature sensor.
+
+}
+adc_ic_t;
+
+__attribute__((always_inline)) static inline void ADC_SetVoltageReference( adc_vr_t vr )
+{
+  ADMUX = (ADMUX & ~MASK2(REFS1,REFS0)) | (((vr & B11) >> 0) << REFS0);
+}
 
 __attribute__((always_inline)) static inline void ADC_SetInputChannel( adc_ic_t ic )
 {
