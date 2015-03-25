436fd40d2d3a995ef1479bf2e6c2e22da2d3882b
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2015-03-25 15:07:35 -0400

    PUE register now controlled by part, not it's own #define

--- cores/tiny/wiring_digital.c
+++ cores/tiny/wiring_digital.c
@@ -32,7 +32,7 @@
 #include "core_timers.h"
 #include "PwmTimer.h"
 
-#if defined( USE_PUE_REGISTER )
+#if defined(__AVR_ATtiny1634__) || defined(__AVR_ATtiny1634__)
 void pinMode(uint8_t pin, uint8_t mode)
 {
   uint8_t bit = digitalPinToBitMask(pin);
@@ -169,7 +169,7 @@ __attribute__((always_inline)) static inline void turnOffPWM( uint8_t pin )
     }
 }
 
-#if defined( USE_PUE_REGISTER )
+#if defined(__AVR_ATtiny1634__) || defined(__AVR_ATtiny1634__)
 
 void digitalWrite(uint8_t pin, uint8_t val)
 {
