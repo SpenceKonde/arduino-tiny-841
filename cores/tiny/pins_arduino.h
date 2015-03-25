3502e44d79da10a81606d31f535fd3e93bd916f0
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2015-03-25 15:06:10 -0400

    Update pins_arduino.h
    add 1634. 
    
    Change USE_PUE_REGISTER to check for part, instead of setting that define and then checking it.

--- cores/tiny/pins_arduino.h
+++ cores/tiny/pins_arduino.h
@@ -44,7 +44,6 @@
 #define PORT_B_ID 2
 #endif
 #if defined( __AVR_ATtinyX41__ )
-#define USE_PUE_REGISTER 1
 #define PORT_A_ID 1
 #define PORT_B_ID 2
 #define SS      3
@@ -96,9 +95,7 @@ extern const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[];
 #define portModeRegister(P) ( (volatile uint8_t *)( pgm_read_byte( port_to_mode_PGM + (P))) )
 #define portPcMaskRegister(P) ( (volatile uint8_t *)( pgm_read_byte( port_to_pcmask_PGM + (P))) )
 
-#if defined( USE_PUE_REGISTER )
-#define portPullupRegister(P) ( (volatile uint8_t *)( pgm_read_byte( port_to_pullup_PGM + (P))) )
-#endif
+
 
 #if defined(__AVR_ATtinyX5__)
 #define digitalPinToPCICR(p)    (((p) >= 0 && (p) <= 5) ? (&GIMSK) : ((uint8_t *)NULL))
@@ -121,6 +118,31 @@ extern const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[];
 #define digitalPinToPCMSKbit(p) (((p) <= 2) ? (p) : (10 - (p)))
 #endif
 
+#if defined(__AVR_ATtiny1634__)
+#define PORT_A_ID 1
+#define PORT_B_ID 2
+#define PORT_C_ID 3
+
+#define digitalPinToPCX(p,s1,s2,s3,s4,s5,s6) \
+    (((p) >= 0) \
+        ? (((p) <   1) ? (s1)  /*  0  -  0  ==>  B0      */  \
+        : (((p) <=  8) ? (s2)  /*  1  -  8  ==>  A7 - A0 */  \
+        : (((p) <= 10) ? (s3)  /*  9  - 10  ==>  C5 - C4 */  \
+        : (((p) <= 13) ? (s4)  /*  11 - 13  ==>  C2 - C0 */  \
+        : (((p) <= 16) ? (s5)  /*  14 - 16  ==>  B3 - B1 */  \
+        : (s6))))) \
+        : (s6))
+//                                                   s1 b     s2 A     s3 C     s3 C     s5 B
+#define digitalPinToPCICR(p)    digitalPinToPCX( p, &GIMSK,  &GIMSK,  &GIMSK,  &GIMSK,  &GIMSK,  NULL )
+#define digitalPinToPCICRbit(p) digitalPinToPCX( p, PCIE1,   PCIE0,   PCIE2,   PCIE2,   PCIE1,   0    )
+#define digitalPinToPCMSK(p)    digitalPinToPCX( p, &PCMSK1, &PCMSK0, &PCMSK2, &PCMSK2, &PCMSK1, NULL )
+#define digitalPinToPCMSKbit(p) digitalPinToPCX( p, p,       8-p,     14-p,    13-p,    17-p,    0    )
+
+
+
+#endif
+
+
 #if defined(__AVR_ATtiny4313__)
 #define digitalPinToPCX(p,s1,s2,s3,s4,s5) \
     (((p) >= 0) \
@@ -135,6 +157,10 @@ extern const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[];
 #define digitalPinToPCICRbit(p) digitalPinToPCX( p, PCIE2,   PCIE1,   PCIE2,   PCIE0,   0    )
 #define digitalPinToPCMSK(p)    digitalPinToPCX( p, &PCMSK2, &PCMSK1, &PCMSK2, &PCMSK0, NULL )
 #define digitalPinToPCMSKbit(p) digitalPinToPCX( p, p,       3-p,     p-2,     p-9,     0    )
+#endif
+
+#if defined(__AVR_ATtiny1634__) || defined(__AVR_ATtiny1634__)
+#define portPullupRegister(P) ( (volatile uint8_t *)( pgm_read_byte( port_to_pullup_PGM + (P))) )
 #endif
 
 #endif
