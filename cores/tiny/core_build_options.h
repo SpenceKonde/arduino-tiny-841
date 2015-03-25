81c91507bed43f72d8bcb5a339dc27b11fbceef2
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2015-03-25 15:01:07 -0400

    Update core_build_options.h
    add 1634

--- cores/tiny/core_build_options.h
+++ cores/tiny/core_build_options.h
@@ -125,6 +125,31 @@
 
 #endif
 
+/*=============================================================================
+  Build options for the ATtiny1634 processor
+=============================================================================*/
+#if defined( __AVR_ATtiny1634__ )
+/*
+  The old standby ... millis on Timer 0.
+*/
+#define TIMER_TO_USE_FOR_MILLIS                   0
+
+/*
+  Tone goes on whichever timer was not used for millis.
+*/
+#if TIMER_TO_USE_FOR_MILLIS == 1
+#define TIMER_TO_USE_FOR_TONE                     0
+#else
+#define TIMER_TO_USE_FOR_TONE                     1
+#endif
+
+#define HAVE_ADC                                  1
+
+#define DEFAULT_TO_TINY_DEBUG_SERIAL              0
+
+
+
+#endif
 
 /*=============================================================================
   Build options for the ATtiny85 processor
